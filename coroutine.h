c8d6f790697aa88f4781748cb3240e78d37723b0
RZHuangJeff <qwerty6933898@gmail.com>
2021-04-12 10:03:53 +0800

    Abstract usage of coroutine
    The original version of coroutine was high flexable, but someone
    may misuse controlling macros, such as declaring several coroutines
    that was wrapped by several cr_begin and cr_end pairs in a single
    loop, which will cause unexpected behavior.
    To avoid such misuing, several changes were introduced.
    First of all, a macro named cr_func_dec is introduced, every
    coroutine should be declared with such macro, which will provides a
    coroutine function with a single argument named __ct, which is a
    pointer points to corresponding cr structure. The first argument of
    following macros: cr_begin, cr_end, cr_wait, cr_exit and cr_sys,
    was removed and reference to it in body of those macros is hardcoded
    as __ct. Such change will limits macros mentioned above in a
    function declared by macro cr_func_dec.
    The declaration of a coroutine context is abstracted, too. Such
    declaration is wrapped by macro cr_context, which helps to declare
    a context that will be used in specific coroutine.
    After declaration of a coroutine function and its corresponding
    context was done, a coroutine can be invoked by macro cr_run.
    To pass arguments to and reference them in coroutine, field local
    defined in structure cr is mentioned. With macro cr_context_init,
    one would be able to specify field local to point to desired
    argument. To reference them with in coroutine, one of following two
    macros could be used: cr_priv_var and cr_grab_priv_var. Former will
    cast __ct->local to a pointer of given type, while later reports
    the address of field within a structure that __ct->local points to.
    Finally, to declare a static local variable, macro cr_local helps
    to highlight such variables, which makes one to figure out whether
    a variable is used between several coroutine cycles or just for
    temporary usage.

--- coroutine.h
+++ coroutine.h
@@ -20,38 +20,60 @@ struct cr {
     void *local; /* private local storage */
 };
 
-#define cr_init()                           \
-    {                                       \
-        .label = NULL, .status = CR_BLOCKED \
+#define cr_context_name(name) cr_context_##name
+
+#define cr_context(name) struct cr cr_context_name(name)
+
+#define cr_context_init(loc)    \
+    {                           \
+        .label = NULL,          \
+        .status = CR_BLOCKED,   \
+        .local = loc            \
     }
-#define cr_begin(o)                     \
-    do {                                \
-        if ((o)->status == CR_FINISHED) \
-            return;                     \
-        if ((o)->label)                 \
-            goto *(o)->label;           \
+#define cr_begin()                          \
+    do {                                    \
+        if ((__ct)->status == CR_FINISHED)  \
+            return;                         \
+        if ((__ct)->label)                  \
+            goto *(__ct)->label;            \
     } while (0)
 #define cr_label(o, stat)                                   \
     do {                                                    \
         (o)->status = (stat);                               \
         __cr_line(label) : (o)->label = &&__cr_line(label); \
     } while (0)
-#define cr_end(o) cr_label(o, CR_FINISHED)
+#define cr_end() cr_label(__ct, CR_FINISHED)
 
 #define cr_status(o) (o)->status
 
-#define cr_wait(o, cond)         \
-    do {                         \
-        cr_label(o, CR_BLOCKED); \
-        if (!(cond))             \
-            return;              \
+#define cr_wait(cond)               \
+    do {                            \
+        cr_label(__ct, CR_BLOCKED); \
+        if (!(cond))                \
+            return;                 \
     } while (0)
 
-#define cr_exit(o, stat)   \
-    do {                   \
-        cr_label(o, stat); \
-        return;            \
+#define cr_exit(stat)           \
+    do {                        \
+        cr_label(__ct, stat);   \
+        return;                 \
     } while (0)
+
+#define cr_func_name(name) cr_func_##name
+
+#define cr_func_def(name) \
+    static void cr_func_name(name)(struct cr *__ct)
+
+#define cr_run(name) cr_func_name(name)(&cr_context_name(name))
+
+#define cr_track_until(name, cond) \
+    (cr_status(&cr_context_name(name)) != (cond))
+
+#define cr_grab_priv_var(type, memb) &((type*) (__ct->local))->memb
+
+#define cr_priv_var(type) ((type *) (__ct->local))
+
+#define cr_local static
 
 #define cr_queue(T, size) \
     struct {              \
@@ -73,8 +95,8 @@ struct cr {
     (cr_queue_empty(q) ? NULL : &(q)->buf[(q)->r++ % cr_queue_len(q)])
 
 /* Wrap system calls and other functions that return -1 and set errno */
-#define cr_sys(o, call)                                                     \
-    cr_wait(o, (errno = 0) || !(((call) == -1) &&                           \
+#define cr_sys(call)                                                        \
+    cr_wait((errno = 0) || !(((call) == -1) &&                              \
                                 (errno == EAGAIN || errno == EWOULDBLOCK || \
                                  errno == EINPROGRESS || errno == EINTR)))
 
--- tinync.c
+++ tinync.c
@@ -12,47 +12,70 @@
 
 typedef cr_queue(uint8_t, 4096) byte_queue_t;
 
-static void stdin_loop(struct cr *o, byte_queue_t *out)
+typedef struct socket_write_local_s {
+    byte_queue_t *in;
+    int fd;
+    uint8_t *b;
+}swl_t;
+
+typedef struct socket_read_local_s {
+    int fd;
+    int r;
+    uint8_t b;
+} srl_t;
+
+cr_func_def(stdin_read)
 {
-    static uint8_t b;
-    static int r;
-    cr_begin(o);
+    byte_queue_t *q = cr_priv_var(byte_queue_t);
+
+    cr_local uint8_t b;
+    cr_local int r;
+
+    cr_begin();
     for (;;) {
-        cr_sys(o, r = read(STDIN_FILENO, &b, 1));
+        cr_sys(r = read(STDIN_FILENO, &b, 1));
         if (r == 0) {
-            cr_wait(o, cr_queue_empty(out));
-            cr_exit(o, 1);
+            cr_wait(cr_queue_empty(q));
+            cr_exit(CR_FINISHED);
         }
-        cr_wait(o, !cr_queue_full(out));
-        cr_queue_push(out, b);
+
+        cr_wait(!cr_queue_full(q));
+        cr_queue_push(q, b);
     }
-    cr_end(o);
+    cr_end();
 }
 
-static void socket_write_loop(struct cr *o, int fd, byte_queue_t *in)
+cr_func_def(socket_write)
 {
-    static uint8_t *b;
-    cr_begin(o);
+    byte_queue_t *in = *cr_grab_priv_var(swl_t, in);
+    int fd = *cr_grab_priv_var(swl_t, fd);
+
+    cr_local uint8_t *b;
+
+    cr_begin();
     for (;;) {
-        cr_wait(o, !cr_queue_empty(in));
+        cr_wait(!cr_queue_empty(in));
         b = cr_queue_pop(in);
-        cr_sys(o, send(fd, b, 1, 0));
+        cr_sys(send(fd, b, 1, 0));
     }
-    cr_end(o);
+    cr_end();
 }
 
-static void socket_read_loop(struct cr *o, int fd)
+cr_func_def(socket_read)
 {
-    static uint8_t b;
-    static int r;
-    cr_begin(o);
+    int fd = *cr_priv_var(int);
+
+    cr_local uint8_t b;
+    cr_local int r;
+
+    cr_begin();
     for (;;) {
-        cr_sys(o, r = recv(fd, &b, 1, 0));
+        cr_sys(r = recv(fd, &b, 1, 0));
         if (r == 0)
-            cr_exit(o, 1);
-        cr_sys(o, write(STDOUT_FILENO, &b, 1));
+            cr_exit(CR_FINISHED);
+        cr_sys(write(STDOUT_FILENO, &b, 1));
     }
-    cr_end(o);
+    cr_end();
 }
 
 static int nonblock(int fd)
@@ -101,13 +124,18 @@ int main(int argc, char *argv[])
     };
     connect(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_in));
 
-    struct cr cr_stdin = cr_init();
-    struct cr cr_socket_read = cr_init();
-    struct cr cr_socket_write = cr_init();
     byte_queue_t queue = cr_queue_init();
+    swl_t socket_write_local = {
+        .fd = fd,
+        .in = &queue
+    };
 
-    while (cr_status(&cr_stdin) == CR_BLOCKED &&
-           cr_status(&cr_socket_read) == CR_BLOCKED) {
+    cr_context(stdin_read) = cr_context_init(&queue);
+    cr_context(socket_read) = cr_context_init(&fd);
+    cr_context(socket_write) = cr_context_init(&socket_write_local);
+
+    while (cr_track_until(stdin_read, CR_FINISHED) &&
+           cr_track_until(socket_read, CR_FINISHED)) {
         if (cr_queue_empty(&queue)) {
             fd_set fds;
             FD_ZERO(&fds);
@@ -115,9 +143,9 @@ int main(int argc, char *argv[])
             FD_SET(fd, &fds);
             select(fd + 1, &fds, NULL, NULL, NULL);
         }
-        socket_read_loop(&cr_socket_read, fd);
-        socket_write_loop(&cr_socket_write, fd, &queue);
-        stdin_loop(&cr_stdin, &queue);
+        cr_run(socket_read);
+        cr_run(socket_write);
+        cr_run(stdin_read);
     }
 
     close(fd);
